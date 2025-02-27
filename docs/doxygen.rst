4c49f5f4ceaef8c9a146fc4c6260390317d42583
Joshua Dahl <joshuadahl@unr.edu>
2025-02-26 21:43:47 -0800

    Documentation
    Adds documentation and an MIT License.
    + Renamed Operations to Instructions
    + Some spelling fixes!

---
+++ docs/doxygen.rst
@@ -0,0 +1,12 @@
+Generated Documentation
+=======================
+
+.. doxygennamespace:: mizu
+   :project: mizu_doxygen
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- mizu/serialize.hpp
+++ mizu/serialize.hpp
@@ -1,24 +1,38 @@
 #pragma once
 
-#include "../operations/lookup.hpp"
+#include "../instructions/lookup.hpp"
 #include "opcode.hpp"
+#include <cassert>
 
 namespace mizu { inline namespace serialization {
+	/**
+	 * Converts a Mizu \p program into a byte array ready to be written to a file or sent over the network.
+	 * @note This function makes no account of different machine endianness or pointer sizes. 
+	 *
+	 * @param program The program to serialize
+	 * @return fp::dynarray<std::byte> a dynamically allocated array of bytes representing the serialized program 
+	 */
 	inline fp::dynarray<std::byte> to_binary(fp::view<const opcode> program) {
 		auto out = fp::dynarray<std::byte>{}.resize(program.size() * sizeof(opcode));
 		std::memcpy(out.raw, program.data(), out.size());
 		for(auto& op: fp::dynarray<opcode>{(opcode*)out.raw})
-			op.op = (operation_t)lookup_id(op.op).value_or(-1);
+			op.op = (instruction_t)lookup_id(op.op).value_or(-1);
 		return out;
 	}
-	inline fp::dynarray<std::byte> to_binary(fp::view<opcode> program) { return to_binary((fp::view<const opcode>&)program); }
 
+	/**
+	 * Converts a blob of \p binary data into a Mizu program
+	 * @note This function makes no account of different machine endianness or pointer sizes. 
+	 * 
+	 * @param binary The binary blob to deserialize
+	 * @return fp::dynarray<opcode> a dynamically allocated Mizu program
+	 */
 	inline fp::dynarray<opcode> from_binary(fp::view<const std::byte> binary) {
+		assert(binary.size() % sizeof(opcode) == 0);
 		auto out = fp::dynarray<opcode>{}.resize(binary.size() / sizeof(opcode));
 		std::memcpy(out.raw, binary.data(), binary.size());
 		for(auto& op: out)
 			op.op = lookup_pointer((size_t)op.op).value_or(nullptr);
 		return out;
 	}
-	inline fp::dynarray<opcode> from_binary(fp::view<std::byte> binary) { return from_binary((fp::view<const std::byte>&)binary); }
 }}
