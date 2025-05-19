45306c679660a671f32221be22f7adf410c57442
Joshua Dahl <joshuadahl@unr.edu>
2025-05-19 16:25:41 -0700

    Windows and Mac Fixes

--- windows.cpp
+++ windows.cpp
@@ -3,10 +3,10 @@
 extern "C" int dll_main(int argc, char** argv);
 
 int main(int argc, char** argv) {
-    // Set the input and output code page to UTF-8 (65001)
-    SetConsoleCP(65001);  // Set console input code page to UTF-8
-    SetConsoleOutputCP(65001);  // Set console output code page to UTF-8
+	// Set the input and output code page to UTF-8 (65001)
+	SetConsoleCP(65001);  // Set console input code page to UTF-8
+	SetConsoleOutputCP(65001);  // Set console output code page to UTF-8
 
-    return dll_main(argc, argv);
+	return dll_main(argc, argv);
 }
 
